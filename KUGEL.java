da821800e0cd55f61e7c8cfd33901c0bffd23f81
Christian Haubner <134379818+HerrHaubner@users.noreply.github.com>
2024-06-09 16:56:27 +0200

    Add files via upload

--- KUGEL.java
+++ KUGEL.java
@@ -1,5 +1,15 @@
-public class KUGEL extends KREIS{
+import ea.edu.*;
+
+public class KUGEL extends KREIS implements KollisionsReagierbar{
     public KUGEL(){
         super();
     }
+    
+    public boolean kollisionReagieren(EduActor ea){
+        
+        if(ea.getClass().getName().equals("KUGEL")) {   System.out.println("Eine Kugel wurde berührt");}
+        return true;   
+    }
+    
+
 }
--- SPIELMITLEVELN.java
+++ SPIELMITLEVELN.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class MARIO here.
+ * Beispiel für Spiel mit verschiedenen Leveln
  *
  * @author (your name)
  * @version (a version number or a date)
@@ -8,9 +8,6 @@
 public class SPIELMITLEVELN extends SPIEL
 {
 
-    /**
-     * Constructor for objects of class MARIO
-     */
     LEVEL1 l1;
     LEVEL2 l2; 
 
@@ -18,16 +15,24 @@ public class SPIELMITLEVELN extends SPIEL
     {
         super(1280,800,true);   //Spiel mit Fenster erzeugen
 
-        new KREIS();         //Bei Bedarf erste Szene einrichten
+        new KREIS();            //Bei Bedarf erste Szene einrichten
         LEVEL.setzeSpiel(this); // Das Spiel bei den Leveln anmelden
 
     }
 
-    public void level1(){        //Levelerzeugung nur außerhalb des Konstruktors möglich sonst Fehler
+    public void level1erzeugen(){        //Levelerzeugung nur außerhalb des Konstruktors möglich sonst Fehler
         l1 = new LEVEL1();
     }
 
-    public void level2(){
+    public void level2erzeugen(){
         l2 = new LEVEL2();
+    }
+    
+    public void level1anzeigen(){        //Levelerzeugung nur außerhalb des Konstruktors möglich sonst Fehler
+        setzeAktiveSzene("LEVEL1");
+    }
+    
+    public void levelwanzeigen(){        //Levelerzeugung nur außerhalb des Konstruktors möglich sonst Fehler
+        setzeAktiveSzene("LEVEL2");
     }
 }
